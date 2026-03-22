62ea7733bee32ce299a980de7b940a273923868b
Luis-Alvarezz <luisangelug10@gmail.com>
2026-03-22 16:03:21 -0600

    [MODIFY]: Se agrego STATE para manejo de informacion Ciudad y Pais

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,10 +1,32 @@
 // * rfc -> Para crear la plantilla basica
+import React, { useState, type ChangeEvent } from 'react'
+import type { SearchType } from '../../types/type'
 import { countries } from '../../data/Coutries'
 import styles from './Form.module.css'
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    // console.log('desde handleChange', e.target.value)
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => { // * Tipado de 'e' lo tomamos de colocar en el Form onSubmit(e=>)
+    e.preventDefault()
+    if (Object.values(search).includes('')) {
+      console.log('Si hay Campos Vacios')
+    }
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.field}>
         <label htmlFor="city">Ciudad</label>
         <input
@@ -12,13 +34,20 @@ export default function Form() {
           type="text"
           name='city'
           placeholder='Ciudad'
+          value={search.city}
+          onChange={handleChange}
         />
       </div>
 
       <div className={styles.field}>
         <label htmlFor="country">País</label>
-        <select name="country">
-          <option id="country">-- Seleccione un País --</option>
+        <select
+          id='country'
+          name="country"
+          value={search.country}
+          onChange={handleChange}
+          >
+          <option>-- Seleccione un País --</option>
           { countries.map(((country) => (
               <option key={country.code} value={country.code}>{country.name}</option>
             ))) }
