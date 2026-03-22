bc23fffd38c7590ed43454df9b3d9291c8f1cb62
Luis-Alvarezz <luisangelug10@gmail.com>
2026-03-22 13:40:14 -0600

    [MODIFY]: Se renderizo componente de Form con su Modulo CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-
+// import "./App.module.css" // * NO FUNCIONA porque NO es dependencia
+import styles from './App.module.css'
+import Form from './components/form/Form'
 
 function App() {
 
   return (
     <>
-      <h1>Buscador de Clima</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        {/* <p>1</p> */}
+        <Form />
+        <p>2</p>
+      </div>
     </>
   )
 }
