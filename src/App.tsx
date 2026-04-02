b5f681fac3b79b620024c509d85d6a2e319525f0
Luis-Alvarezz <luisangelug10@gmail.com>
2026-04-02 00:16:45 -0600

    [ADD]: State via destructuring para renderizar el Componente de Error

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Form from './components/Form/Form'
 import Spinner from './components/Spinner/Spinner'
 import WeatherDetail from './components/WeatherDetail/WeatherDetail'
 import useWeather from './hooks/useWeather'
+import { Error } from './components/Errors/Error'
 
 function App() {
 
-  const { fetchWeather, weather, hasWeatherData, loading } = useWeather()
+  const { fetchWeather, weather, hasWeatherData, loading, notFound } = useWeather()
 
   return (
     <>
@@ -27,6 +28,9 @@ function App() {
           <WeatherDetail
             weather={weather}
           />
+        }
+        {
+          notFound && <Error>Ciudad No Encontrada</Error>
         }
       </div>
     </>
